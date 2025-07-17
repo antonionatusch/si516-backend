21290e0ceb182dd4a50ef4c7265e0bd39b26646c
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2025-07-16 20:30:29 -0400

    feat: initial commit

---
+++ src/main/java/com/si516/saludconecta/SaludconectaApplication.java
@@ -0,0 +1,13 @@
+package com.si516.saludconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludconectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludconectaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/si516/saludconecta/SaludconectaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si516.saludconecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludconectaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
