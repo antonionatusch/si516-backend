7cce827872159a89e9d399a1901d34e2bc233c41
ANTONIO MIGUEL NATUSCH ZARCO <a2022111958@estudiantes.upsa.edu.bo>
2025-07-18 14:35:52 -0400

    feat(service): add office and patient service files
    Faltaría agregar los DTOs, mappers y repositorios en un futuro. Ver: #11, #12, #15

---
+++ src/main/java/com/si516/saludconecta/service/OfficeService.java
@@ -0,0 +1,9 @@
+package com.si516.saludconecta.service;
+
+import com.si516.saludconecta.document.Office;
+
+import java.util.List;
+
+public interface OfficeService {
+    //TODO: Crear DTOs y Mappers.
+}
---
+++ src/main/java/com/si516/saludconecta/service/PatientService.java
@@ -0,0 +1,5 @@
+package com.si516.saludconecta.service;
+
+public interface PatientService {
+    //TODO: Crear DTOs y Mappers.
+}
