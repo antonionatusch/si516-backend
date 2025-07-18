e792163c2a42cdebc04df36e367ab9af08facc94
ANTONIO MIGUEL NATUSCH ZARCO <a2022111958@estudiantes.upsa.edu.bo>
2025-07-18 16:16:34 -0400

    feat(service): add initial PatientService method signatures

--- src/main/java/com/si516/saludconecta/service/PatientService.java
+++ src/main/java/com/si516/saludconecta/service/PatientService.java
@@ -1,5 +1,13 @@
 package com.si516.saludconecta.service;
 
+import com.si516.saludconecta.dto.PatientDTO;
+
+import java.util.List;
+
 public interface PatientService {
-    //TODO: Crear DTOs y Mappers.
+    List<PatientDTO> getAll();
+    PatientDTO getById(String id);
+    PatientDTO create(PatientDTO patient);
+    PatientDTO update(String id, PatientDTO patient);
+    void delete(String id);
 }
