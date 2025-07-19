e3f3ab723d8443ab5b951b87d0566923f66dc2fa
Antonio Natusch <a2022111958@estudiantes.upsa.edu.bo>
2025-07-18 23:18:26 -0400

    feat(service): add initial file storage service method signatures

---
+++ src/main/java/com/si516/saludconecta/service/FileStorageService.java
@@ -0,0 +1,22 @@
+package com.si516.saludconecta.service;
+
+import com.si516.saludconecta.dto.file.FileMetadataDTO;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+public interface FileStorageService {
+    FileMetadataDTO storeAudio(MultipartFile file,
+                               String doctorId,
+                               String patientId,
+                               String appointmentId) throws IOException;
+
+    Optional<Resource> loadAsResource(String fileId);
+
+    Optional<FileMetadataDTO> getMetadata(String fileId);
+
+    List<FileMetadataDTO> list(int limit);
+}
